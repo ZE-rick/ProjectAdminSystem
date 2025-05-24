785e36a630da63caac99bde406bb6746341719eb
ZE-rick <erick.usc@outlook.es>
2025-05-24 13:43:53 -0500

    init commit

---
+++ src/main/java/projectadminsystem/ProjectAdminSystemApplication.java
@@ -0,0 +1,13 @@
+package projectadminsystem;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class ProjectAdminSystemApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(ProjectAdminSystemApplication.class, args);
+    }
+
+}
---
+++ src/test/java/projectadminsystem/ProjectAdminSystemApplicationTests.java
@@ -0,0 +1,13 @@
+package projectadminsystem;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class ProjectAdminSystemApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
